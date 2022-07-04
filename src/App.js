d7946b301574466f57a3ffd401e7b7a363e3baad
umair2607 <umair.k2607@gmail.com>
2022-07-04 23:09:26 +0500

    added routing for components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import List from "./Pages/List/List";
+import New from "./Pages/New/New";
+import Single from "./Pages/Single/Single";
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
